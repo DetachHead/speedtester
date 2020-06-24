4d0348fd215e1730afbb23cc63cefa228da87671
KotlinIsland <65446343+KotlinIsland@users.noreply.github.com>
2020-06-24 13:19:22 +1000

    convert to Deno (#1)
    * Delete yarn.lock
    
    * Update and rename main.js to main.ts
    
    * Delete package.json
    
    * Create settings.ts
    
    * Update README.md

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { default as settings } from "./settings.ts";
+
+const url =
+  `https://speedtest-${settings.location}.vocus.net:8080/download?&size=${settings.size}`;
+console.log("starting infinite speedtest at " + url);
+let i = 1;
+setInterval(async () => {
+  try {
+    await fetch(url);
+    console.log("success #" + i);
+  } catch (err) {
+    console.log(`error #${i}:`);
+    console.log(err);
+  } finally {
+    i++;
+  }
+}, settings.frequency);
---
+++ settings.ts
@@ -0,0 +1,5 @@
+export default {
+    "frequency": 500,
+    "location": "syd",
+    "size": 10000 
+}
